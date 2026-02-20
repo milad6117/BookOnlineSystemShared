924576bcb90101a7260e62b9f32afe8f6ff1fa0c
Milad <milad.611717@gmail.com>
2026-02-20 12:55:57 -0800

    Initial commit: Add Shared project and GitHub Actions workflow

---
+++ Pagination/PagedData.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ProjectManagement.Framework.Pagination
+{
+    public class PagedData<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public PaginationMetadata Metadata { get; set; }
+
+        public PagedData(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            Items = items;
+            Metadata = new PaginationMetadata
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+            };
+        }
+
+        // در کلاس PagedData
+        // برای وقتی که میخوایم از Dapper استفاده کنیم
+        public static PagedData<T> Create(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            return new PagedData<T>(items.ToList(), count, pageNumber, pageSize);
+        }
+    }
+
+}
---
+++ Pagination/QueryableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Framework.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookOnline.Shared.Pagination
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<PagedData<T>> ToPagedListAsync<T>(
+      this IQueryable<T> source,
+      int pageNumber,
+      int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedData<T>(items, count, pageNumber, pageSize);
+        }
+
+        // متد جدید (Overload) برای راحتی کار با PaginationParams
+        public static Task<PagedData<T>> ToPagedListAsync<T>(
+            this IQueryable<T> source,
+            PaginationParams @params)
+        {
+            return source.ToPagedListAsync(@params.PageNumber, @params.PageSize);
+        }
+    }
+}
